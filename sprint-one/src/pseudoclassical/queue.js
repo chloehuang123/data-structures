91d69189d439921fe09ca1179fb7608fee0512a2
chloe <312864100@qq.com>
2022-01-14 10:49:46 -0500

    Complete pseudoclassical stack and queue

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,25 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.start = 0;
+  this.end = 0;
 };
+
+Queue.prototype.enqueue = function (val) {
+  this.storage[this.end] = val;
+  this.end++;
+}
+
+Queue.prototype.dequeue = function () {
+  var unWanted = this.storage[this.start];
+  delete this.storage[this.start];
+  this.start++;
+  return unWanted;
+}
+
+Queue.prototype.size = function () {
+  return Object.keys(this.storage).length;
+}
 
 
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,27 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.l = 0;
 };
+
+Stack.prototype.push = function (val) {
+  this.storage[this.l] = val;
+  this.l++;
+}
+
+Stack.prototype.pop = function () {
+  if (this.storage.length === 0) {
+    return null;
+  }
+  var unWanted = this.storage[this.l-1];
+  delete this.storage[this.l-1];
+  this.l--;
+  return unWanted;
+}
+
+Stack.prototype.size = function () {
+  return Object.keys(this.storage).length;
+}
 
 
