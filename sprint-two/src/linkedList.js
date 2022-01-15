8c399290ceb1a2c35c728f212facae977d40317a
chloe <312864100@qq.com>
2022-01-15 16:25:56 -0500

    working on linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,10 +4,39 @@ var LinkedList = function () {
   list.tail = null;
 
   list.addToTail = function (value) {
+    //generate a node
+    // need to check whether the head exists already
+    // if there isnt a head then the new node will be the head and the tail
+    // head = tail = node for the first node in the list
+    // after the first node then we can add to head or tail
+    // must replace the last nodes .next property with the one we add to the tail
+    //class of value is a Node
+    //var a = Node('a');
+    //l = LinkedList();
+    //l.addToTail(a);
 
+    var newNode = Node(value);
+
+    if (!list.head) {
+      list.head = newNode;
+      list.tail = newNode;
+    } else {
+      list.tail = newNode;
+      list.tail.next = newNode;
+    }
+    return list;
   };
 
   list.removeHead = function () {
+    if(!list.head) {
+      //remove head
+      var returnNode = list.head;
+      // delete and replace
+      list.head = returnNode.next;
+      return returnNode.value;
+    } else {
+      return list.head;
+    }
   };
 
   list.contains = function (target) {
