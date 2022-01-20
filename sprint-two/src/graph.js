5d799f89c6ad5c4d3af85a56b319e762230d4f12
chloe <312864100@qq.com>
2022-01-19 20:36:19 -0500

    complete graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -22,7 +22,23 @@ Graph.prototype.contains = function(node) {
 Graph.prototype.removeNode = function(node) {
   var index = this.nodes.indexOf(node);
   delete this.nodes[index];
+
+ // check if the node exists in the this.edge
+  if(Object.keys(this.edges).includes(node)) {
+    // if so, delete
+    delete this.edges.node;
+  }
+  // iterate through the keys
+  for (var keys in this.edges) {
+    var index = this.edges[keys].indexOf(node);
+    if (index > -1) {
+      delete this.edges[keys][index];
+    }
+  }
 };
+
+
+
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
@@ -42,16 +58,32 @@ Graph.prototype.addEdge = function(fromNode, toNode) {
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
   // search for the fromNode from the keys
+  // delete the fromNodes from the toNodes
   if (this.edges[fromNode]) {
-    // delete toNode from the array
-    // must also delete fromNode from the array
     var index = this.edges[fromNode].indexOf(toNode);
     delete this.edges[fromNode][index];
   }
+  if (this.edges[toNode]) {
+    var index = this.edges[toNode].indexOf(fromNode);
+    delete this.edges[toNode][index];
+  }
+
 };
+
+
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
+  //input: function
+  // iterate through each node
+  for (var i = 0; i < this.nodes.length; i++) {
+    // invoke cb function
+    // node is at this.nodes[i]
+    // have to invoke the function on this.nodes[i]
+    // 3.push() instead of graph.push()
+    // arguments
+    cb.call(this, this.nodes[i]);
+  }
 };
 
 /*
