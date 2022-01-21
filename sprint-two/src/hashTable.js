28aab2ffd1a79f741dc1429b4f3ca668c017bc76
chloe <312864100@qq.com>
2022-01-20 22:22:18 -0500

    working on hashTables

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -6,15 +6,29 @@ var HashTable = function() {
 };
 
 HashTable.prototype.insert = function(k, v) {
+  debugger;
   var index = getIndexBelowMaxForKey(k, this._limit);
+  //check if the index is taken
+  if (k) {
+    // if not, then we use set function
+    this._storage.set(index, v);
+    // else
+  } else {
+    // generate a new index
+    newIndex = getIndexBelowMaxForKey(k+1, this._limit);
+    // use set function again
+    this._storage.set(newIndex, v);
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  return this._storage.get(index);
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.set(index, undefined);
 };
 
 
